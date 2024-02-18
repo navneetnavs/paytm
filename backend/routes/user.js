4b7f1343309d26e8309c6c66b296bd54ec86763a
navneetnavs <navneetkumar2004@icloud.com>
2024-02-18 08:26:41 +0530

    commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,7 +9,7 @@ const { JWT_SECRET } = require("../config");
 const signupBody = zod.object({
     username : zod.string().email(),
     firstName : zod.string(),
-    lastName : zod.string();
+    lastName : zod.string(),
     password: zod.string()
 })
 
@@ -43,7 +43,33 @@ router.post("/signup", async(req,res)=>{
         token : token
     })
 })
+router.post ("/signin", async(req,res)=>{
+    const {success} = signinBody.safeParse(req.body)
+    if(!success) {
+        return res.status(411).json({
+            message : "Email already taken"
+        })
+    }
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
 
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+
+    res.status(411).json({
+        message: "Error while logging in"
+})
+})
 
 
 
